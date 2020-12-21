b42cb978c6ded395cb7e5fc6548be24ab5cff4d6
Александр <kaf1@mail.ru>
2020-12-21 21:17:49 +0300

    bild

---
+++ build/precache-manifest.d893aeb87534eddd453d46c564e2ed93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1900ce3d97561cef3620ba5e0ba82c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee44c624abeeae2f54c",
+    "url": "/static/css/main.40c9e18d.chunk.css"
+  },
+  {
+    "revision": "b57a07c4f9f1aff01763",
+    "url": "/static/js/2.2cc910b0.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.2cc910b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee44c624abeeae2f54c",
+    "url": "/static/js/main.900bc981.chunk.js"
+  },
+  {
+    "revision": "cf93728d1d004e7382fb",
+    "url": "/static/js/runtime-main.a89ad4bc.js"
+  }
+]);
