37c8b95f6f4e569a5ef05899745e810f3285e127
Александр <kaf1@mail.ru>
2020-12-21 22:52:53 +0300

    Updates

---
+++ precache-manifest.3eaeab419353f14ac9a975c1c72de1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545dc1bd446d8276f944f16d3590dc5b",
+    "url": "/Diplom_in_JS/index.html"
+  },
+  {
+    "revision": "b6f14d67e49794321641",
+    "url": "/Diplom_in_JS/static/css/main.40c9e18d.chunk.css"
+  },
+  {
+    "revision": "b57a07c4f9f1aff01763",
+    "url": "/Diplom_in_JS/static/js/2.2cc910b0.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/Diplom_in_JS/static/js/2.2cc910b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f14d67e49794321641",
+    "url": "/Diplom_in_JS/static/js/main.b4c91416.chunk.js"
+  },
+  {
+    "revision": "eae8e845e014802c893d",
+    "url": "/Diplom_in_JS/static/js/runtime-main.daf8d1a1.js"
+  }
+]);
